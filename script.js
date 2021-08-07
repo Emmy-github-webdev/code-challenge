90ff0d8d2c724a2262a5aec14ee5fd090095f7de
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-08-07 11:29:49 +0000

    commit FizzBuzz code challenge

---
+++ script.js
@@ -0,0 +1,34 @@
+
+/*
+#1. Fizz Buzz using Modulus Operator
+
+num divisible y 3 = Fizz
+num divisible by 5 - Buzz
+num divisible by 3 and 5 - FizzBuzz
+*/
+
+
+
+/* function sayFizzBuss(num){
+    
+    for(let i = 1; i <= num; i++){
+     if(i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz');
+     }else if(i % 3 === 0){
+            console.log('Fizz');
+        }else if(i % 5 === 0){
+            console.log('Buzz');
+        }else{
+            console.log(i);
+        }
+    };
+};
+
+sayFizzBuss(30); */
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
