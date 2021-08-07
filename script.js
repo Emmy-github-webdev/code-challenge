6c4edb1b40286002439d6ae91b3280ae92eaaf7e
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-08-07 13:10:38 +0000

    commit Caeser Cipher code

--- script.js
+++ script.js
@@ -76,3 +76,49 @@ harmlessRansomNote('this is a secret note for you from a secret admirer',
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+
+/*
+#3 Caesar Cipher
+
+*/
+
+/* function caesarCipher(str, num){
+
+    num = num % 26;
+    let lowerCaseString = str.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let newString ='';
+
+
+    for(i = 0; i < lowerCaseString.length; i++){
+        let currentLetter = lowerCaseString[i];
+        if(currentLetter === ' '){
+            newString += currentLetter;
+            continue;
+        }
+
+        let currentIndex = alphabet.indexOf(currentLetter);
+        let newIndex = currentIndex + num;
+
+        if(newIndex > 25) newIndex = newIndex - 26;
+        if(newIndex < 0) newIndex = 26 + newIndex;
+
+        if(str[i] === str[i].toUpperCase()){
+            newString += alphabet[newIndex].toUpperCase();
+        } else newString += alphabet[newIndex];
+    };
+
+    console.log(newString);
+    //return newString;
+}
+
+//caesarCipher('Zoo Keeper', 2);
+
+//caesarCipher('Big Car', -16);
+
+caesarCipher('Javascript', -900); */
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
